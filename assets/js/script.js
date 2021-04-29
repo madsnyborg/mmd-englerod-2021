a1b11feb05a3df2f1513bc9598d040848bedfa17
charlottegracia <1086120@ucn.dk>
2021-04-29 16:05:22 +0200

    noget virker hjælp

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,10 @@ const apiUserCredentials = {
 };
 
 const CategoryId = 10; // Opskriftindex
+const opskriftId = 7; // ikke en bestemt opskrift
 
+const morgenmadId = 6;
+const mindreRetterId = 7;
 
 //Error message
 function errorMessage(msg) {
@@ -56,23 +59,19 @@ function createPage() {
 
 function getCategoriesFromWP() { //gets categories from wordpress
   const xhttp = new XMLHttpRequest();
+  const url = window.location.href;
   xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) { // this is ok
       try {
         CategoryPosts = JSON.parse(this.response); //converts data to json and puts it in CategoryPosts variable
         console.log(CategoryPosts);
-        let categoryList = '';
-        for (let i = 0; i < CategoryPosts.length; i++){ // for loop that draws the categories with title and picture
-            const item = CategoryPosts[i];
-            categoryList += `
-                <a href="opskriftsindex.html?pageId=${CategoryPosts[i].id}" id="gridItem" class="gridItem${i}" style="background-image: url(${CategoryPosts[i].acf.billede_til_kategori.url})">
-                    <div class="imgTxt">
-                        <p>${CategoryPosts[i].acf.titel_pa_kategori}</p>
-                    </div>
-                </a>
-              `;
+        if (url.indexOf('pageId') == -1) {
+          drawCategories(CategoryPosts);
+        } else {
+          findPageId(url);
+          drawSpecificCategory(pageId, CategoryPosts);
         }
-        document.querySelector('.mainGrid').innerHTML = categoryList; //sends categoryList-string to mainGrid in html
+
       } catch (error) {
         errorMessage(`Parsing error:${error}`);
       }
@@ -95,8 +94,104 @@ function getCategoriesFromWP() { //gets categories from wordpress
   xhttp.send();
 }
 
-/*pageId = findPageId();
+function findPageId(url) {
+  const urlSplit = url.split('?');
+  
+  if (urlSplit[1].indexOf('&') == -1){
+      const parameterSplit = urlSplit[1].split('=');
+      pageId = parameterSplit[1];
+  } /* else {
+      const urlParameters = urlSplit[1].split('&');
+      for (let i = 0; i < urlParameters.length; i++) {
+          if (urlParameters[i].substring(0,6) == 'pageId'){
+              const pageIdSplit = urlParameters[i].split('=');
+              pageId = pageIdSplit[1];
+              break;
+          }
+      }
+  }*/
+  return pageId;
+}
 
-function findPageId() {
+function drawCategories(CategoryPosts) {
+  let categoryList = '';
+        for (let i = 0; i < CategoryPosts.length; i++){ // for loop that draws the categories with title and picture
+            categoryList += `
+                <a href="opskriftsindex.html?pageId=${CategoryPosts[i].id}" id="gridItem" class="gridItem${i}" style="background-image: url(${CategoryPosts[i].acf.billede_til_kategori.url})">
+                    <div class="imgTxt">
+                        <p>${CategoryPosts[i].acf.titel_pa_kategori}</p>
+                    </div>
+                </a>
+              `;
+        }
+        document.querySelector('.mainGrid').innerHTML = categoryList; //sends categoryList-string to mainGrid in html
+}
 
+function drawSpecificCategory(pageId, CategoryPosts) {
+  console.log(pageId);
+  let text = "";
+  CategoryPosts.forEach(category => {
+    if (category.id == pageId){
+      text += `<h1>${category.acf.titel_pa_kategori}</h1>`
+      document.querySelector('main').innerHTML = text;
+    }
+  });
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) { // this is ok
+      try {
+        opskrifter = JSON.parse(this.response); //converts data to json and puts it in CategoryPosts variable
+        console.log(opskrifter);
+
+      } catch (error) {
+        errorMessage(`Parsing error:${error}`);
+      }
+    }
+    if (this.readyState === 4 && this.status >= 400) { // error
+      errorMessage("An error has occured, please try again later.");
+    }
+  };
+
+  // where to send the request and how?
+  xhttp.open("GET", `${apiUrl}posts?status=private&categories=${morgenmadId}&per_page=50`, true);
+
+  // specify any necassary request headers
+  xhttp.setRequestHeader(
+    "Authorization",
+    `Bearer ${window.localStorage.getItem("authToken")}`
+  );
+
+  // send request
+  xhttp.send();
+}
+
+  //console.log(opskrifter);
+
+/*function hejsa() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = () => {
+    if (this.readyState === 4 && this.status === 200) { // this is ok
+      try {
+        opskrifter = JSON.parse(this.response); //converts data to json and puts it in opskrifter variable
+        console.log(opskrifter);
+      } catch (error) {
+        errorMessage(`Parsing error:${error}`);
+      }
+    }
+    if (this.readyState === 4 && this.status >= 400) { // error
+      errorMessage("An error has occured, please try again later.");
+    }
+  };
+  // where to send the request and how?
+  xhttp.open("GET", `${apiUrl}posts?status=private&categories=${opskriftId}&per_page=50`, true);
+  // specify any necassary request headers
+  xhttp.setRequestHeader(
+    "Authorization",
+    `Bearer ${window.localStorage.getItem("authToken")}`
+  );
+  // send request
+  xhttp.send();
+  return opskrifter;
 } */
+
